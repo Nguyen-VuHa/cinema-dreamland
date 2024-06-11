7c734492f845745cbd429cbdb2be91e1b7a871ab
Nguyen-VuHa <havu20111999@gmail.com>
2024-06-11 22:01:01 +0700

    custom input password

--- src/app/auth/sign-up/page.js
+++ src/app/auth/sign-up/page.js
@@ -1,7 +1,12 @@
 'use client'
+import Link from 'next/link';
 import React from 'react';
+import FacebookLoginButton from '~/components/pages/SignIn/FacebookButton';
+import GoogleLoginButton from '~/components/pages/SignIn/GoogleButton';
+import Button from '~/components/ui/Button';
 import Input from '~/components/ui/Input';
 import InputDate from '~/components/ui/InputDate';
+import InputPassword from '~/components/ui/InputPassword';
 
 function SignUpPage() {
     return (
@@ -10,14 +15,41 @@ function SignUpPage() {
                 className='w-full'
                 placeholder="Email"
             /> 
+            <InputPassword 
+                className='w-full'
+                placeholder="Mật Khẩu"
+                type="password"
+            /> 
             <Input 
                 className='w-full'
                 placeholder="Họ & Tên"
             />
             <InputDate 
                 className='w-full'
-                placeholder="Ngày sinh nhat"
+                placeholder="Sinh nhật"
             />
+            <Input 
+                className='w-full'
+                placeholder="Số điện thoại"
+            />
+            <div className='flex space-x-2'>
+                <span className='text-input-place'>Bạn đã có tài khoản?</span>
+                <Link href="/auth/sign-in" className='text-primary hover:underline transition-all'>
+                    Đăng nhập
+                </Link>
+            </div>
+            <div className='divide-y divide-primary space-y-3'>
+                <Button
+                    // type="submit"
+                >
+                    Đăng Ký
+                </Button>
+                <div />
+            </div>
+            <div className='flex items-center space-x-2'>
+                <GoogleLoginButton />
+                <FacebookLoginButton />
+            </div>
         </form>
     );
 }
---
+++ src/components/ui/InputPassword.js
@@ -0,0 +1,62 @@
+'use client';
+
+import React, { useState } from 'react';
+import { CiLock, CiUnlock  } from "react-icons/ci";
+
+function InputPassword({ className, classNameInput, placeholder, inputChange, value, errMessage, children }, ref) {
+
+    const [passwordShow, setPasswordShow] = useState(false)
+
+    return (
+        <div className={
+            (className || '') + 
+            ` w-full space-y-2 relative`
+        }>
+            <input 
+                className={classNameInput || '' + 
+                    `w-full border-2 border-solid border-transparent
+                    rounded-md shadow-0 bg-layout-second text-[white]
+                    px-3 py-2
+                    transition-all
+                    font-text
+                    focus:!border-primary
+                    placeholder-input-place
+                    hover:bg-hover hover:transition-all
+                    `
+                }
+                placeholder={placeholder || 'Nhập gì đó . . .'}
+                ref={ref}
+                onChange={(e) => {
+                    inputChange && inputChange(e.target.value, e)
+                }}
+                value={value}
+                type={passwordShow ? 'text' : 'password'}
+                {...children}
+            />
+            {/* Button View Password */}
+            <div
+                className='
+                    absolute top-0 m-0 right-[3%]
+                    flex justify-center items-center
+                    cursor-pointer p-1 text-[20px] text-input-place rounded-lg
+                    transition-all
+                    hover:text-primary hover:bg-hover
+                '
+                onClick={() => {
+                    setPasswordShow(!passwordShow)
+                }}
+            >
+                {
+                    passwordShow ?  <CiUnlock />
+                    : <CiLock />
+                }
+               
+            </div>  
+            {
+                errMessage && <small className='text-error font-extralight italic'>{ errMessage }</small>
+            }
+        </div>
+     )
+}
+
+export default  React.forwardRef(InputPassword);
